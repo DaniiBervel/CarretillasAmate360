39d2d96792f845b6d5e612afd1c90b4541d587ff
DaniiBervel <150744226+DaniiBervel@users.noreply.github.com>
2024-09-30 10:06:08 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,100 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": 0.14414503461193462,
+        "pitch": 0.10750323601688194,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.061189324255453315,
+          "pitch": 0.20764709509357893,
+          "rotation": 0,
+          "target": "2-2"
+        },
+        {
+          "yaw": 1.909606919564057,
+          "pitch": 0.18017832234565923,
+          "rotation": 0,
+          "target": "1-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.632281287426169,
+          "pitch": 0.07752394346128,
+          "title": "TIZONA 35",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-3",
+      "name": "3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": 3.13895954033735,
+        "pitch": 0.011075101974959978,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": 0.07971017442155315,
+        "pitch": 0.17384108865003256,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
