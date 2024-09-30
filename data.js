5e826783e7e0c2ddb735638bc6b5dc5be45c4477
DaniiBervel <150744226+DaniiBervel@users.noreply.github.com>
2024-09-30 12:51:17 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,138 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.19446988838556933,
+        "pitch": 0.16532945425725032,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.119123688783988,
+          "pitch": 0.3167399814616232,
+          "rotation": 0,
+          "target": "1-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.1258511400793889,
+          "pitch": 0.2629584328821757,
+          "rotation": 0,
+          "target": "0-1"
+        },
+        {
+          "yaw": 1.9149918458736144,
+          "pitch": 0.29288702300324587,
+          "rotation": 0,
+          "target": "2-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.610355514548985,
+          "pitch": 0.07171273870146422,
+          "title": "Tizona 35",
+          "text": "<font color=\"#000000\" face=\"Verdana, Arial, Helvetica, sans-serif\"><span style=\"font-size: 11px;\">EQUIPO DE PULVERIZACION AUTOPROPULSADO 4x4 - Pulverización y Fumigación intensiva en Invernadero</span></font><br>"
+        }
+      ]
+    },
+    {
+      "id": "2-3",
+      "name": "3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 3.0689199893215147,
+        "pitch": -0.0051027021253791105,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.18004253288171412,
+          "pitch": 0.2030402444310475,
+          "rotation": 0,
+          "target": "1-2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
